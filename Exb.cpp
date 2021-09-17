92c55606dad04dd0eb4ecf702fc2694e81488e8a
DFerranti <dferrantisc@hotmail.com>
2021-09-17 17:18:27 -0400

    updt

--- Exb.cpp
+++ Exb.cpp
@@ -1,24 +1,22 @@
 //Para cada item abaixo, faça um programa paralelo em C++, empregando funçoes MPI, no modelo CGM, que:
-
 // b) Encontre o maior e o menor valor de um conjunto S de n elementos, onde n mod p=0. A entrada deverá ser fornecida por meio de um arquivo e a saída deverá ser emitida no terminal.
 
 #include <iostream>
 #include <stdio.h>
-//#include <mpi.h>
+#include <mpi.h>
 
 using std::cin;
 using std::cout;
 using std::endl;
 
-//#define TAM;
-
 int main(int argc, char *argv[])
 {
-    int TAM =0;
+    int TAM = 0;
     int vetS[TAM], i;
     int nMaior, nMenor, n;
+    int rank, size;
 
-    cout << "Informe o Tamanho do Conjunto S:"<<endl;
+    cout << "Informe o Tamanho do Conjunto S:" << endl;
     cin >> TAM;
 
     //Preencher o Conjunto S;
@@ -26,8 +24,10 @@ int main(int argc, char *argv[])
     {
         cout << "Informe os valores do Conjunto S:" << endl;
         cin >> vetS[i];
+        cout << endl;
     }
-    //Achar o Numero Maior
+
+    //Achar o Numero Maior e Menor.
     nMaior = vetS[0];
     nMenor = vetS[0];
     for (i = 1; i <= TAM; i++)
@@ -44,14 +44,13 @@ int main(int argc, char *argv[])
     cout << "Numero Maior:" << nMaior << endl;
     cout << "Numero Menor:" << nMenor << endl;
 
-    /* int rank, size;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     printf("Rank: %d/%d\n", rank, size);
 
-    MPI_Finalize();*/
+    MPI_Finalize();
 
     return 0;
 }
